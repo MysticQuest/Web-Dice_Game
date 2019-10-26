01c307278ae18d4d29d224e93a3a6c2bc9055995
Mystic Quest <l1skou@gmail.com>
2019-10-27 00:35:07 +0300

    added Js

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
-var diceArray = [dice1, dice2, dice3, dice4, dice6]
+var numbers = [];
 
-Math.round(Math.random() * 7);
+for (i = 0; i <2 ; i++)  //rolls for each player
+{
+var rand = throwDice();
+numbers.push(rand);      //stores dice results
+setImage();
+}
+
+whoWins();
+
+function throwDice() {
+  var rand = Math.floor(Math.random() * 6) + 1;
+  return rand;
+}
+
+function setImage() {
+var randDice = "dice" + rand + ".png";
+var randImgSource = "images/" + randDice;
+var image = document.querySelectorAll("img")[i];
+image.setAttribute("src", randImgSource);
+}
+
+function whoWins() {
+if (numbers[0] > numbers[1])
+{document.querySelector("h1").innerHTML = "Player 1 Wins!";}
+else if (numbers[0] < numbers[1])
+{document.querySelector("h1").innerHTML = "Player 2 Wins!";}
+else
+{document.querySelector("h1").innerHTML = "Draw!";}
+}
